24b5e1309bf2764165a45f4267b39aed7737dff6
root <root@pop-os.localdomain>
2021-03-02 22:11:35 -0300

    Initial setup

---
+++ src/main/java/com/studies/topdeliver/TopdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.studies.topdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopdeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studies/topdeliver/TopdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.topdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
